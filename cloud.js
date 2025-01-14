e31d449a11b698e11c4e6f996765f96bba530d55
dudu <11677326+dudu-m@user.noreply.gitee.com>
2025-01-14 02:39:11 +0000

    remove av.cloud

--- cloud.js
+++ cloud.js
@@ -5,30 +5,16 @@ const path = require('path')
 /**
  * Loads all cloud functions under the `functions` directory.
  */
-fs.readdirSync(path.join(__dirname, 'functions')).forEach( file => {
-  require(path.join(__dirname, 'functions', file))
-})
+// fs.readdirSync(path.join(__dirname, 'functions')).forEach( file => {
+//   require(path.join(__dirname, 'functions', file))
+// })
 
 /**
  * A simple cloud function.
  */
-// AV.Cloud.define('hello', function(request) {
-//   return 'Hello world!'
-// })
-
-// 用户注册
-// AV.Cloud.define('register', async (request) => {
-//   const { username, password } = request.params;
-//   const user = new AV.User();
-//   user.setUsername(username);
-//   user.setPassword(password);
-//   try {
-//     await user.signUp();
-//     return { success: true };
-//   } catch (error) {
-//     throw error;
-//   }
-// });
+AV.Cloud.define('hello', function(request) {
+  return 'Hello world!'
+})
 
 // 用户登录状态检查中间件
 function requireAuth(req, res, next) {
